f0fe98d0992287d0db56f1c9e3806c42e208c459
JZwlth <wkjazy87@gmail.com>
2024-10-27 20:02:09 -0700

    update

--- Goat.h
+++ Goat.h
@@ -11,13 +11,13 @@ private:
     int age;
     string color;
 public: 
-
+    // Constructors
     Goat() { name = ""; age = 0; color = ""; } 
     Goat(string n) : name(n), age(0), color("Unknown") {} 
     Goat(string n, int a) : name(n), age(a), color("Unknown") {}
     Goat(string n, int a, string c) : name(n), age(a), color(c) {}
 
-
+    // Setters and Getters
     void set_name(string n) { name = n; }
     string get_name() const { return name; }
     void set_age(int a) { age = a; }
@@ -25,12 +25,12 @@ public:
     void set_color(string c) { color = c; }
     string get_color() const { return color; }
 
-
+    // Overloaded < operator for std::list sorting
     bool operator<(const Goat &other) const {
         return name < other.name;
     }
 
-
+    // Display function for Goat
     void display() const {
         cout << name << " (" << age << ", " << color << ")";
     }
